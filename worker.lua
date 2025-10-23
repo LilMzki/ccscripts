0593e0bcbeec9030cfa29f49570a8cc2ecbba6a2
LilMzki <Mi2uk1ikz12d@gmail.com>
2025-10-23 11:17:08 +0900

    Implement turtle control logic in worker.lua

---
+++ worker.lua
@@ -0,0 +1,41 @@
+print("Hello. Enter position x value of this turtle.")
+
+xPos = tonumber(read())
+
+rednet.open("right")
+local id = rednet.lookup("Turtle3DPrinter", "Master")
+
+if id then
+    rednet.send(id, string.format("first %s", xPos));
+    print(rednet.receive());
+    print("please enter 'yes' if inventory setup done")
+    input = read()
+    if(input == "yes") then
+        rednet.send(id, "second")
+        print("wait for every turtle to have done setting up")
+        local rawCommands = rednet.receive()
+        for i=1,#rawCommands do
+            local firstChar = string.sub(rawCommands, i, i)
+            if firstChar == "f" then
+                turtle.forward()
+            elseif firstChar == "t" then
+                turtle.turnRight()
+                turtle.turnRight()
+            elseif firstChar == "u" then
+                turtle.up()
+            elseif firstChar == "p" then
+                turtle.placeDown()
+            elseif firstChar == "s" then
+                local thirdChar = string.sub(rawCommands, i+2, i+2)
+                local slotId = 0
+                if tonumber(thirdChar) then
+                    slotId = tonumber(string.sub(rawCommands, i+1, i+2))
+                else
+                    slotId = tonumber(string.sub(rawCommands, i+1, i+1))
+                end
+                turtle.select(slotId)
+            end
+        end
+        print("done")
+    end
+end
